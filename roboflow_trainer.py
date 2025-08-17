a26e76b2764ac376ffdd431b2f0d788ae512dd18
USMANHEART <usmanham33d@live.com>
2025-08-17 14:01:27 +0800

    feat: roboflow train

--- roboflow_trainer.py
+++ roboflow_trainer.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 from roboflow import Roboflow
 from rfdetr import RFDETRBase
+from os.path import join, exists
 
 # 1️⃣ Configuration
 API_KEY = "zxp6OFr5Vhj6Qa6JUjQi"
 PROJECT_NAME = "cubicasa5k-2-qpmsa-gbbqv"
 VERSION = "2"
-DATASET_DIR = Path("dataset/coco")
-OUTPUT_DIR = Path("trained_model")
+
 
 EPOCHS = 50
 BATCH_SIZE = 4
@@ -17,40 +17,66 @@ DEVICE = "cuda"  # Change to "cpu" if no GPU
 EARLY_STOPPING = True
 
 
-def download_dataset():
+def get_paths():
+    project_dir = Path(__file__).resolve().parent.resolve()
+    dataset_dir = Path(join(project_dir, "dataset"))
+    coco_dataset = Path(join(dataset_dir, "coco"))
+    output_dir = Path(join(dataset_dir, "output"))
+    output_dir.mkdir(parents=True, exist_ok=True)
+    dataset_dir.mkdir(parents=True, exist_ok=True)
+    coco_dataset.mkdir(parents=True, exist_ok=True)
+    return {
+        "dir": project_dir,
+        "output": output_dir,
+        "coco": coco_dataset,
+        "dataset": dataset_dir,
+    }
+
+
+def download_dataset(paths: dict):
+    dataset_dir: Path = paths["coco"]
+    is_filled = any(dataset_dir.iterdir())
     """Downloads dataset from Roboflow if dataset directory is empty."""
-    if not DATASET_DIR.exists() or not any(DATASET_DIR.iterdir()):
+    if not is_filled:
         print("Dataset not found or empty. Downloading from Roboflow...")
         rf = Roboflow(api_key=API_KEY)
         project = rf.workspace().project(PROJECT_NAME)
-        dataset = project.version(VERSION).download("coco")
-        print(f"Download complete. Dataset saved at: {DATASET_DIR}")
+        dataset = project.version(VERSION).download(
+            "coco",
+            location=str(dataset_dir),
+            overwrite=True
+        )
+        print(f"Download complete. Dataset saved at: {dataset}")
     else:
-        print(f"Dataset already exists at {DATASET_DIR}. Skipping download.")
+        print(f"Dataset already exists at {dataset_dir}. Skipping download.")
 
 
-def train_model():
+def train_model(paths: dict):
     """Trains RF-DETR on the dataset."""
+    dataset_dir: Path = paths["coco"]
+    output_dir: Path = paths["output"]
+    print("train model dataset at:", dataset_dir)
     print("Initializing RF-DETR model...")
     model = RFDETRBase()
 
     print("Starting training...")
     model.train(
-        dataset_dir=str(DATASET_DIR),
+        dataset_dir=str(dataset_dir),
         epochs=EPOCHS,
         batch_size=BATCH_SIZE,
         grad_accum_steps=GRAD_ACCUM_STEPS,
         lr=LR,
-        output_dir=str(OUTPUT_DIR),
+        output_dir=str(output_dir),
         device=DEVICE,
         early_stopping=EARLY_STOPPING
     )
-    print(f"Training finished. Model checkpoints saved in: {OUTPUT_DIR}")
+    print(f"Training finished. Model checkpoints saved in: {output_dir}")
 
 
 def main():
-    download_dataset()
-    train_model()
+    paths = get_paths()
+    download_dataset(paths)
+    train_model(paths)
 
 
 if __name__ == "__main__":
